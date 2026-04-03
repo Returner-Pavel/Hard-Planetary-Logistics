ad50d4a5b0f9fe6e7a4ba2d78b2cdff674b9c626
Returner_org <apavel72hd.kuba@outlook.com>
2026-04-03 17:39:42 +0300

    Добавление Tools для интеграций других модов

--- tool.lua
+++
@@ -1,7 +0,0 @@
-local surface_conditions = require("__Hard-Planetary-Logistics__.tools.surface-conditions")
-
-local Tools = {}
-
-Tools["surface_conditions_v1"] = surface_conditions
-
-return Tools
---
+++ tools.lua
@@ -0,0 +1,7 @@
+local surface_conditions_v1 = require("__Hard-Planetary-Logistics__.tools.surface-conditions") 
+
+local Tools = {}
+
+Tools.surface_conditions_v1 = surface_conditions
+
+return Tools
